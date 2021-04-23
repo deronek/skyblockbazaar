14f9ac20f2dd20908d312d449bce9a165f1204d5
deronek <dionek3@gmail.com>
2021-04-23 16:57:58 +0200

    Initial commit

---
+++ src/test/java/com/mattdion/skyblockbazaar/SkyblockbazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattdion.skyblockbazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyblockbazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
