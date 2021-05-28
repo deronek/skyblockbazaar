1c2ad9a45d5918fbb1918c2296fc6579dcbad6d7
deronek <dionek3@gmail.com>
2021-05-28 15:48:09 +0200

    Prototype of MyBazaarReply

--- src/main/java/com/mattdion/skyblockbazaar/SkyblockBazaarApplication.java
+++ src/main/java/com/mattdion/skyblockbazaar/SkyblockBazaarApplication.java
@@ -1,17 +1,10 @@
 package com.mattdion.skyblockbazaar;
 
-import java.util.Arrays;
-
-import com.mattdion.skyblockbazaar.hypixelapi.HypixelAPIUtil;
-import com.mattdion.skyblockbazaar.player.PlayerMap;
-import com.mattdion.skyblockbazaar.player.PlayerMapService;
-import net.hypixel.api.HypixelAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,20 +13,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class SkyblockBazaarApplication {
 
-	@RequestMapping("/test")
-	@ResponseBody
-	String home() {
-		return "Hello World! TEST";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkyblockBazaarApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkyblockBazaarApplication.class, args);
-	}
+    @RequestMapping("/test")
+    @ResponseBody
+    String home() {
+        return "Hello World! TEST";
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			System.out.println("App started");
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("App started");
+        };
+    }
 }
